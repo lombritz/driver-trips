6e40bc716ed7b13790274359698b342eabc2b9db
Jaime Rojas <jaime_rojas@claro.com.do>
2016-05-11 16:52:16 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "driver-trips"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "net.sourceforge.htmlunit" % "htmlunit" % "2.21"
---
+++ src/main/scala/Credentials.scala
@@ -0,0 +1,8 @@
+/**
+  * @author Jaime Rojas
+  * @date 5/11/2016
+  */
+object Credentials {
+  val EMAIL = "ubersti2@gmail.com"
+  val PASSWORD = "Ubersti234"
+}
